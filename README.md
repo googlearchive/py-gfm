e2768bf28bffea83b9fa09dffb9e17462f1e7c59
Nathan Weizenbaum <nweiz@google.com>
2012-12-28 19:23:58 -0800

    Add an initial GFM extension.
    This extension just enables all the built-in extensions that are GFM-compatible.

---
+++ README.md
@@ -0,0 +1,6 @@
+This is an implementation of [GitHub-Flavored Markdown][gfm] written as an
+extension to the Python [Markdown](md) library. It aims for maximal
+compatibility with GitHub's rendering.
+
+[gfm]: http://github.github.com/github-flavored-markdown/
+[md]: http://packages.python.org/Markdown/
---
+++ mdx_gfm/__init__.py
@@ -0,0 +1,21 @@
+# Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
+# for details. All rights reserved. Use of this source code is governed by a
+# BSD-style license that can be found in the LICENSE file.
+
+from markdown.extensions import Extension
+from markdown.extensions.fenced_code import FencedCodeExtension
+from markdown.extensions.nl2br import Nl2BrExtension
+from markdown.extensions.smart_strong import SmartEmphasisExtension
+from markdown.extensions.tables import TableExtension
+
+def makeExtension(configs=None):
+    return GithubFlavoredMarkdownExtension(configs=configs)
+
+class GithubFlavoredMarkdownExtension(Extension):
+    """An extension that's as compatible as possible with GFM."""
+
+    def extendMarkdown(self, md, md_globals):
+        FencedCodeExtension().extendMarkdown(md, md_globals)
+        Nl2BrExtension().extendMarkdown(md, md_globals)
+        SmartEmphasisExtension().extendMarkdown(md, md_globals)
+        TableExtension().extendMarkdown(md, md_globals)
