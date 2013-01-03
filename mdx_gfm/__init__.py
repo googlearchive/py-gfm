ff8f3a35bc3495c7863570181eb7769886399094
Nathan Weizenbaum <nweiz@google.com>
2013-01-02 17:12:21 -0800

    Add a module for the GFM variant used for Markdown files.

--- mdx_gfm/__init__.py
+++ mdx_gfm/__init__.py
@@ -2,32 +2,27 @@
 # for details. All rights reserved. Use of this source code is governed by a
 # BSD-style license that can be found in the LICENSE file.
 
-from markdown.extensions import Extension
-from markdown.extensions.fenced_code import FencedCodeExtension
 from markdown.extensions.nl2br import Nl2BrExtension
-from markdown.extensions.smart_strong import SmartEmphasisExtension
-from markdown.extensions.tables import TableExtension
 
-import gfm
+from mdx_partial_gfm import PartialGithubFlavoredMarkdownExtension
 
 def makeExtension(configs=None):
     return GithubFlavoredMarkdownExtension(configs=configs)
 
-class GithubFlavoredMarkdownExtension(Extension):
-    """An extension that's as compatible as possible with GFM."""
+class GithubFlavoredMarkdownExtension(PartialGithubFlavoredMarkdownExtension):
+    """An extension that's as compatible as possible with GFM.
+
+    This extension aims to be compatible with the standard GFM that GitHub uses
+    for comments and issues. It has all the extensions described in the `GFM
+    documentation`_, except for intra-Github links to commits, repostiories, and
+    issues.
+
+    Note that Markdown-formatted gists and files (including READMEs) on GitHub
+    use a slightly different variant of GFM. For that, use
+    :class:`~mdx_partial_gfm.PartialGithubFlavoredMarkdownExtension`.
+    """
 
     def extendMarkdown(self, md, md_globals):
-        # Built-in extensions
-        FencedCodeExtension().extendMarkdown(md, md_globals)
-        Nl2BrExtension().extendMarkdown(md, md_globals)
-        SmartEmphasisExtension().extendMarkdown(md, md_globals)
-        TableExtension().extendMarkdown(md, md_globals)
+        PartialGithubFlavoredMarkdownExtension.extendMarkdown(self, md, md_globals)
 
-        # Custom extensions
-        gfm.AutolinkExtension().extendMarkdown(md, md_globals)
-        gfm.AutomailExtension().extendMarkdown(md, md_globals)
-        gfm.HiddenHiliteExtension([
-            ('guess_lang', 'False'),
-            ('css_class', 'highlight')
-        ]).extendMarkdown(md, md_globals)
-        gfm.SemiSaneListExtension().extendMarkdown(md, md_globals)
+        Nl2BrExtension().extendMarkdown(md, md_globals)
---
+++ mdx_partial_gfm/__init__.py
@@ -0,0 +1,42 @@
+# Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
+# for details. All rights reserved. Use of this source code is governed by a
+# BSD-style license that can be found in the LICENSE file.
+
+from markdown.extensions import Extension
+from markdown.extensions.fenced_code import FencedCodeExtension
+from markdown.extensions.smart_strong import SmartEmphasisExtension
+from markdown.extensions.tables import TableExtension
+
+import gfm
+
+def makeExtension(configs=None):
+    return PartialGithubFlavoredMarkdownExtension(configs=configs)
+
+class PartialGithubFlavoredMarkdownExtension(Extension):
+    """An extension that's as compatible as possible with GFM.
+
+    This extension aims to be compatible with the variant of GFM that GitHub
+    uses for Markdown-formatted gists and files (including READMEs). This
+    variant seems to have all the extensions described in the `GFM
+    documentation`_, except:
+
+    - Newlines in paragraphs are not transformed into ``br`` tags.
+    - Intra-Github links to commits, repositories, and issues are not supported.
+
+    .. _the GFM documentation: http://github.github.com/github-flavored-markdown
+    """
+
+    def extendMarkdown(self, md, md_globals):
+        # Built-in extensions
+        FencedCodeExtension().extendMarkdown(md, md_globals)
+        SmartEmphasisExtension().extendMarkdown(md, md_globals)
+        TableExtension().extendMarkdown(md, md_globals)
+
+        # Custom extensions
+        gfm.AutolinkExtension().extendMarkdown(md, md_globals)
+        gfm.AutomailExtension().extendMarkdown(md, md_globals)
+        gfm.HiddenHiliteExtension([
+            ('guess_lang', 'False'),
+            ('css_class', 'highlight')
+        ]).extendMarkdown(md, md_globals)
+        gfm.SemiSaneListExtension().extendMarkdown(md, md_globals)
