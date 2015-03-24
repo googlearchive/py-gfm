b3256df415b58c7ce03022fbeb767ad5ebde60f9
Natalie Weizenbaum <nweiz@google.com>
2015-03-24 13:09:42 -0700

    Update setup.py and setup.cfg in preparation for a PyPi release.
    Closes #14

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='py-gfm',
-    version='0.1',
+    version='0.1.0',
     description='An implementation of Github-Flavored Markdown written as an extension to the Python Markdown library.',
-    author='Dart',
+    author='Dart Team',
+    author_email='misc@dartlang.org',
     url='https://github.com/dart-lang/py-gfm',
+    download_url='https://github.com/dart-lang/py-gfm/tarball/0.1.0',
     packages=find_packages(),
     include_package_data = True,
     install_requires = ['setuptools', 'markdown', 'unittest2'],
